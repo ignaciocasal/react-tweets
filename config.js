85bf35cbccffb6e131c2bd18e6a25ff163310c62
Juan Ignacio Casal <ignaciocasal@gmail.com>
2014-12-02 02:45:55 -0200

    removed twt credentials

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: '75rm5piyDF64aibKV9bIHZCxw',
-    consumer_secret: 'UiOXMVx5j4TC3HFk9kx6ZUGYRqwfoDnD8UgT8DJDOFqam0vCei',
-    access_token_key: '412320498-1InYFyjWsRh9AxLJDJQzSCUT9PARINXZf5Arx8Jz',
-    access_token_secret: 'OeJ9t23u2iVLnIAX2BzipKd5LCCBdRkgM6S4sar3wwtEr'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
   }
 }
